6b8ea9fac59d4ef89a533b14cfe39c6a28c71a58
khactuan2401 <63339576+khactuan2401@users.noreply.github.com>
2020-04-20 22:26:31 +0700

    Delete Baitap2.py

--- Baitap2.py
+++
@@ -1,9 +0,0 @@
-#Tính giờ phút giây
-t=int(input("Nhập vào số giây :"))
-h=(t//3600)%24
-m=(t%3600)//60
-s=(t%3600)%60
-print("So gio la :",h)
-print("So phut la:",m)
-print("So giay la",s)
-print(h,":",m,":",s)
